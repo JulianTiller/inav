ace80accfacdfbd4b5261747f09968eb5f5544f5
Scavanger <andreas.kanzler@outlook.com>
2023-11-01 09:43:32 -0300

    FW-Autoland

--- src/main/target/common.h
+++ src/main/target/common.h
@@ -147,7 +147,8 @@
 //#define USE_POWER_LIMITS //n.V. in INAV2.4.0
 
 #define NAV_FIXED_WING_LANDING
-#define USE_SAFE_HOME //n.V. in INAV2.4.0
+#define USE_SAFE_HOME
+#define USE_FW_AUTOLAND
 #define USE_AUTOTUNE_FIXED_WING
 #define USE_LOG
 #define USE_STATS
