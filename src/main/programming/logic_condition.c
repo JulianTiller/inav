ce6491b3dfb9def16cb20dc7807ec6e7c0c97e5c
Darren Lines <darren@darrenlines.uk>
2024-06-02 16:01:52 +0100

    Add battery profile to programming framework

--- src/main/programming/logic_condition.c
+++ src/main/programming/logic_condition.c
@@ -806,6 +806,10 @@ static int logicConditionGetFlightOperandValue(int operand) {
             return getConfigProfile() + 1;
             break;
 
+        case LOGIC_CONDITION_OPERAND_FLIGHT_BATT_PROFILE: //int
+            return getConfigBatteryProfile() + 1;
+            break;
+        
         case LOGIC_CONDITION_OPERAND_FLIGHT_ACTIVE_MIXER_PROFILE: // int
             return currentMixerProfileIndex + 1;
             break;
