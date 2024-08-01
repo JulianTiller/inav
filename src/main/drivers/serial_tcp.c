403596443969658950eb787f33650861454a3050
MUSTARDTIGERFPV <mustardtiger@thetacoduck.com>
2024-08-01 02:35:42 +0000

    Allow serialpassthrough to set parity & stop bits

--- src/main/drivers/serial_tcp.c
+++ src/main/drivers/serial_tcp.c
@@ -318,6 +318,12 @@ void tcpSetMode(serialPort_t *instance, portMode_t mode)
     UNUSED(mode);
 }
 
+void tcpSetOptions(serialPort_t *instance, portOptions_t options)
+{
+    UNUSED(instance);
+    UNUSED(options);
+}
+
 static const struct serialPortVTable tcpVTable[] = {
     {
         .serialWrite = tcpWrite,
@@ -327,6 +333,7 @@ static const struct serialPortVTable tcpVTable[] = {
         .serialSetBaudRate = tcpSetBaudRate,
         .isSerialTransmitBufferEmpty = isTcpTransmitBufferEmpty,
         .setMode = tcpSetMode,
+        .setOptions = tcpSetOptions,
         .isConnected = tcpIsConnected,
         .writeBuf = tcpWritBuf,
         .beginWrite = NULL,
